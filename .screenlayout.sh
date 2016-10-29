4270e481e8ef8e450467398c7b711559d6d0436b
austburn <austburn@gmail.com>
2016-10-29 17:59:42 -0500

    Change screenlayout again

--- .screenlayout.sh
+++ .screenlayout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --mode 1920x1080 --left-of eDP1 --rotate normal --output DP2 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --mode 1920x1080 --right-of eDP1 --rotate normal --output DP2 --off
